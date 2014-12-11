19de43c2d2590bca96cb6188a64c611f0e0a3808
NosicLin <nosiclin@foxmail.com>
2014-12-11 22:43:23 +0800

    @android ok

--- lib/libfaeris/proj.android/jni/Android.mk
+++ lib/libfaeris/proj.android/jni/Android.mk
@@ -13,7 +13,7 @@ LOCAL_CFLAGS :=     -I$(LOCAL_PATH)/../../src \
  					-I$(LOCAL_PATH)/../../../lib3rdparty/zlib/include \
  					-I$(LOCAL_PATH)/../../../lib3rdparty/libtga/src \
  					-I$(LOCAL_PATH)/../../../lib3rdparty/minizip/include \
- 					-I$(LOCAL_PATH)/../../../lib3rdparty/curl/include_android \
+ 					-I$(LOCAL_PATH)/../../../lib3rdparty/curl/include/android \
  					-I$(LOCAL_PATH)/../../../lib3rdparty/fmod/include \
  					-I$(LOCAL_PATH)/../../../lib3rdparty/jsonc/src \
  					-I$(LOCAL_PATH)/../../../lib3rdparty/libjpeg/include.android \
