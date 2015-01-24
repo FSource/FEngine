9f71392ed99792d3e2ceec242ac382a9727a7efe
NosicLin <nosiclin@foxmail.com>
2015-01-25 00:47:52 +0800

    @begin to add ios support(60%)

---
+++ lib/lib3rdparty/libpng/proj.ios/xcode/build/libpng.build/Debug-iphonesimulator/libpng.build/Objects-normal/x86_64/pngpread.d
@@ -0,0 +1,5 @@
+dependencies: \
+  /Users/NosicLin/CodeSpace/Faeris/lib/lib3rdparty/libpng/src/pngpread.c \
+  ../../src/pngpriv.h ../../src/png.h ../../src/pnglibconf.h \
+  ../../src/pngconf.h ../../src/pnginfo.h ../../src/pngstruct.h \
+  ../../src/pngdebug.h
---
+++ lib/libfaeris/src/sys/platform/ios/FsWindowIos.m
@@ -0,0 +1,8 @@
+#include "sys/FsWindow.h"
+#include "GL_IOS/gl_ios_es.h"
+#include "graphics/FsRenderDevice.h"
+
+
+
+
+
