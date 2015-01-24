8b6cde1dd04da70a6f61e0f886e2f8d0346a9aa6
NosicLin <nosiclin@foxmail.com>
2015-01-25 00:51:13 +0800

    @remove uused file

--- lib/lib3rdparty/libpng/proj.ios/xcode/build/libpng.build/Release-iphonesimulator/libpng.build/Objects-normal/x86_64/pngwtran.d
+++
@@ -1,5 +0,0 @@
-dependencies: \
-  /Users/NosicLin/CodeSpace/Faeris/lib/lib3rdparty/libpng/src/pngwtran.c \
-  ../../src/pngpriv.h ../../src/png.h ../../src/pnglibconf.h \
-  ../../src/pngconf.h ../../src/pnginfo.h ../../src/pngstruct.h \
-  ../../src/pngdebug.h
