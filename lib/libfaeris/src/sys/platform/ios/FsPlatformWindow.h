16332565c6d48938d7f0cb32a9e9f8ab8605105c
NosicLin <nosiclin@foxmail.com>
2015-01-27 22:16:03 +0800

    @update

---
+++ lib/libfaeris/src/sys/platform/ios/FsPlatformWindow.h
@@ -0,0 +1,20 @@
+#ifndef _FS_PLATFORM_WINDOW_H_
+#define _FS_PLATFORM_WINDOW_H_
+
+#import <UIKit/UIKit.h>
+#import <OpenGLES/EAGL.h>
+#import <OpenGLES/EAGLDrawable.h>
+#import <OpenGLES/ES2/gl.h>
+#import <OpenGLES/ES2/glext.h>
+#import <CoreFoundation/CoreFoundation.h>
+
+@interface FsPlatformWindow:UiView
+{
+
+}
+
+
+
+
+
+#endif /*_FS_PLATFORM_WINDOW_H_*/
